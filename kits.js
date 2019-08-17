a0316752a9c46d0734f6a44a07897677b77ce6da
大司马 <1270433886@qq.com>
2019-08-17 21:20:19 +0800

    Initial commit

--- kits.js
+++ kits.js
@@ -29,19 +29,45 @@ kits.formatDate = function () {
   return year + '-' + month + '-' + day + ' ' + hour + ':' + minute + ':' + second;
 }
 
-kits.randomInt = function(n,m){
-  return Math.floor(Math.random() * (m-n+1) + n);
+// 随机整数
+kits.randomInt = function (n, m) {
+  return Math.floor(Math.random() * (m - n + 1) + n);
 }
 
 // 常见的给id的方式1
 // 当前时间戳 + 大的随机数
-kits.getId = function(){
+kits.getId = function () {
   // 返回一个不容易重复的id
   let date = new Date();
   let time = date.getTime();// 得到的是从1970年1月1日到现在为止的毫秒总数
   // 然后在得到一个足够大的随机数，把毫秒和随机数相连，作为新的id
-  let r = this.randomInt(100000,999999);
+  let r = this.randomInt(100000, 999999);
   // 把两个数字连起来
   let id = time + '' + r;
   return id;
+}
+
+// rgb随机颜色函数
+kits.randomColor = function () {
+  var r = Math.floor(Math.random() * 256);
+  var g = Math.floor(Math.random() * 256);
+  var b = Math.floor(Math.random() * 256);
+  return "rgb(" + r + "," + g + "," + b + ")";
+}
+
+// 随机16进制颜色
+kits.getColor=function() {
+  //定义字符串变量colorValue存放可以构成十六进制颜色值的值
+  var colorValue = "0,1,2,3,4,5,6,7,8,9,a,b,c,d,e,f";
+  //以","为分隔符，将colorValue字符串分割为字符数组["0","1",...,"f"]
+  var colorArray = colorValue.split(",");
+  var color = "#";//定义一个存放十六进制颜色值的字符串变量，先将#存放进去
+  //使用for循环语句生成剩余的六位十六进制值
+  for (var i = 0; i < 6; i++) {
+    //colorArray[Math.floor(Math.random()*16)]随机取出
+    // 由16个元素组成的colorArray的某一个值，然后将其加在color中，
+    //字符串相加后，得出的仍是字符串
+    color += colorArray[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
